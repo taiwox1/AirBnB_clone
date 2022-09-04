5d3949d2e8e572226897695bde34068211889321
Adedotun Taiwo <taiwoadedotunx1@gmail.com>
2022-09-04 22:20:10 +0100

    airBnb project

--- models/base_model.py
+++ models/base_model.py
@@ -46,4 +46,5 @@ class BaseModel:
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
         clname = self.__class__.__name__
+
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
